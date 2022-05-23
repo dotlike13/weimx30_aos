60cf34f8e249efdbf655321a6945795cf7b61e69
Lee byungsun <bslee@coinplug.com>
2022-05-23 14:29:42 +0900

    scheme 수정

--- sdk/src/main/java/com/coinplug/wemixwallet/sdk/RequestSchemeCreator.java
+++ sdk/src/main/java/com/coinplug/wemixwallet/sdk/RequestSchemeCreator.java
@@ -7,9 +7,8 @@ import androidx.annotation.NonNull;
 public class RequestSchemeCreator{
     public static @NonNull Uri create(@NonNull ActivityWrapper activityWrapper, @NonNull String requestId) {
         // Create scheme
-        Uri.Builder appSchemebuilder = new Uri.Builder().scheme("https")
-                .authority("mykeepin.coinplug.com")
-                .path("sign")
+        Uri.Builder appSchemebuilder = new Uri.Builder().scheme("wemix")
+                .authority("wallet")
                 .appendQueryParameter("requestId", requestId);
 
         return appSchemebuilder.build();
